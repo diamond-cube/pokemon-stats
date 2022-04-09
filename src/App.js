2b203a174e3c996e3bdb3997adf0cf99ef6b603f
diamond-cube <iyer.aditya0102@gmail.com>
2022-04-09 17:35:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import "./App.css";
+
+function App() {
+  const [pokemonName, setPokemonName] = useState("");
+  const [chosenPokemon, setChosenPokemon] = useState(false);
+  const [pokemonInfo, setPokemonInfo] = useState({
+    name: "",
+    index: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  });
+
+  function searchPokemon() {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (res) => {
+        setPokemonInfo({
+          name: res.data.name[0].toUpperCase() + res.data.name.substring(1),
+          index: res.data.order,
+          img: res.data.sprites.front_default,
+          hp: res.data.stats[0].base_stat,
+          attack: res.data.stats[1].base_stat,
+          defense: res.data.stats[2].base_stat,
+          type:
+            res.data.types[0].type.name[0].toUpperCase() +
+            res.data.types[0].type.name.substring(1),
+        });
+        setChosenPokemon(true);
+      }
+    );
+  }
+
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>Pokemon Database</h1>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(event) => {
+            setPokemonName(event.target.value);
+          }}
+        />
+        <button onClick={searchPokemon}>Search</button>
+      </div>
+      <div className="display-section">
+        {!chosenPokemon ? (
+          <h1>Please choose a pokemon</h1>
+        ) : (
+          <>
+            <h2>{pokemonInfo.name}</h2>
+            <img src={pokemonInfo.img} />
+            <h3>Index: {pokemonInfo.index}</h3>
+            <h3>Type: {pokemonInfo.type}</h3>
+            <h3>HP: {pokemonInfo.hp}</h3>
+            <h3>Attack: {pokemonInfo.attack}</h3>
+            <h3>Defense: {pokemonInfo.defense}</h3>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
